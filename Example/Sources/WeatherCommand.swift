dc07435a40154bc1b4ca85ed1e125a151afb7d04
Austin Evans <austinjevans@me.com>
2025-05-16 14:06:47 -0400

    Working weather example

--- Example/Sources/WeatherCommand.swift
+++ Example/Sources/WeatherCommand.swift
@@ -106,6 +106,11 @@ typealias Location = String
 struct WeatherService {
   let apiKey: String?
 
+  enum WeatherError: Error {
+    case locationNotFound
+    case apiKeyNotFound
+  }
+
   struct GeocodingResult: Decodable {
     let lat: Double
     let lon: Double
@@ -113,7 +118,7 @@ struct WeatherService {
 
   func fetchWeather(for city: String) async throws -> String {
     guard let apiKey = apiKey else {
-      return "Error: API key not set"
+      throw WeatherError.apiKeyNotFound
     }
 
     let geoURL = URL(
@@ -121,7 +126,11 @@ struct WeatherService {
         "https://api.openweathermap.org/geo/1.0/direct?q=\(city)&limit=1&appid=\(apiKey)"
     )!
     let geoData = try await URLSession.shared.data(from: geoURL).0
-    let loc = try JSONDecoder().decode([GeocodingResult].self, from: geoData).first!
+    let loc = try JSONDecoder().decode([GeocodingResult].self, from: geoData)
+
+    guard let loc = loc.first else {
+      throw WeatherError.locationNotFound
+    }
 
     let oneCallURL = URL(
       string:
